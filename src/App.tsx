7b6087fd1a7c628c048f196be22f56213a0882fe
JotaDiasDevs <joaovitor.semente.d@gmail.com>
2025-10-05 20:22:37 -0300

    feat: configuracao inicial do projeto Vite + React + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Sistema de Controle de Acesso</h1>
+      <p>Projeto inicializado com Vite + React + TypeScript</p>
+    </div>
+  )
+}
+
+export default App
